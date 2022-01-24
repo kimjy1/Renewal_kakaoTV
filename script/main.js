8a198ac21481ae8c25269d5121126d86c6acbed9
kimjy1 <93234891+kimjy1@users.noreply.github.com>
2022-01-24 13:20:19 +0900

    메인 페이지 코딩
    메인 페이지 정렬 완료

--- script/main.js
+++ script/main.js
@@ -17,21 +17,38 @@ $.ajax({
     }
   });
 
-  $.ajax({
-    method: "GET",
-    url: "https://dapi.kakao.com/v2/search/vclip",
-    data: { query: "연합뉴스", size: 30 },
-    headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
-  })
-  
-    .done(function (dl) {
-      alert("라이브 데이터 받아오기 완료");
-      console.log(dl.documents[0].title);
-      console.log(dl.documents[0].url);
-      console.log(dl.documents[0].thumbnail);
-      console.log(dl.documents[0].author);
-  
-      for (let i = 0; i < 12; i++) {
-        $(".livevideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].thumbnail + "'/>" + "</a>");
-      }
-    });
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v2/search/vclip",
+  data: { query: "연합뉴스", size: 30 },
+  headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
+})
+
+  .done(function (dl) {
+    console.log(dl.documents[0].title);
+    console.log(dl.documents[0].url);
+    console.log(dl.documents[0].thumbnail);
+    console.log(dl.documents[0].author);
+
+    for (let i = 0; i < 12; i++) {
+      $(".livevideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].thumbnail + "'/>" + "</a>");
+    }
+  });
+
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v2/search/vclip",
+  data: { query: "셀럽", size: 30 },
+  headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
+})
+
+  .done(function (dl) {
+    console.log(dl.documents[0].title);
+    console.log(dl.documents[0].url);
+    console.log(dl.documents[0].thumbnail);
+    console.log(dl.documents[0].author);
+
+    for (let i = 0; i < 12; i++) {
+      $(".popularvideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].thumbnail + "'/>" + "</a>");
+    }
+  });
