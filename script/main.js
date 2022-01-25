0780401e51deb7778f33a0ff093afb911f71e60d
kimjy1 <93234891+kimjy1@users.noreply.github.com>
2022-01-25 13:18:37 +0900

    prettier 적용
    prettier 적용 완료
    유튜브 api key 발급 완료
    ajax로 요청할 때 쓰는 매개변수 찾는중

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+// 왼쪽 섹션 - 인기 오리지널 비디오&텍스트
 $.ajax({
   method: "GET",
   url: "https://dapi.kakao.com/v2/search/vclip",
@@ -6,49 +7,61 @@ $.ajax({
 })
 
   .done(function (dl) {
-    alert("데이터 받아오기 완료");
-    console.log(dl.documents[0].title);
-    console.log(dl.documents[0].url);
-    console.log(dl.documents[0].thumbnail);
-    console.log(dl.documents[0].author);
-
     for (let i = 0; i < 12; i++) {
       $(".originalvideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].thumbnail + "'/>" + "</a>");
     }
   });
 
+// 오른쪽 섹션 - 인기 라이브 비디오&텍스트
 $.ajax({
   method: "GET",
-  url: "https://dapi.kakao.com/v2/search/vclip",
-  data: { query: "연합뉴스", size: 30 },
+  url: "https://dapi.kakao.com/v2/search/image",
+  data: { query: "카카오쇼핑" },
   headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
 })
 
   .done(function (dl) {
-    console.log(dl.documents[0].title);
-    console.log(dl.documents[0].url);
-    console.log(dl.documents[0].thumbnail);
-    console.log(dl.documents[0].author);
+    alert("카카오쇼핑 데이터 받기 완료")
+    console.log(dl.documents[0].collection);
+    console.log(dl.documents[0].thumbnail_url);
+    console.log(dl.documents[0].image_url);
+    console.log(dl.documents[0].display_sitename);
 
     for (let i = 0; i < 12; i++) {
-      $(".livevideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].thumbnail + "'/>" + "</a>");
+      $(".livevideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].thumbnail_url + "'/>" + "</a>");
     }
   });
 
+// 왼쪽 섹션 - 셀럽들의 라이프스타일 비디오&텍스트
 $.ajax({
   method: "GET",
   url: "https://dapi.kakao.com/v2/search/vclip",
-  data: { query: "셀럽", size: 30 },
+  data: { query: "셀럽들의 라이프스타일", size: 30 },
   headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
 })
 
   .done(function (dl) {
-    console.log(dl.documents[0].title);
-    console.log(dl.documents[0].url);
-    console.log(dl.documents[0].thumbnail);
-    console.log(dl.documents[0].author);
-
     for (let i = 0; i < 12; i++) {
       $(".popularvideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].thumbnail + "'/>" + "</a>");
     }
   });
+
+let apikey = "AIzaSyACk-2UnGwYMn3tx0fm8ubUpKVDqu5BCfU";
+let keyword = "짱구는 못말려";
+
+$.ajax({
+  url: 'https://www.googleapis.com/youtube/v3/search',
+  type: 'get',
+  dataType: 'json',
+  data: { part: 'snippet', key: apikey, q: keyword, maxResults: 50, type: 'video', videoEmbeddable: 'true' },
+  success: function (data) {
+    console.log(data);
+    $.each(data.items, function (i, item) {
+      thumbnail = item.snippet.thumbnails.medium.url; // 썸네일 이미지
+      videoId = item.id.videoId; // 비디오 아이디	
+    });
+  }
+});
+function v_link(videoId) {
+  location.href = 'https://www.youtube.com/watch?v=' + videoId;
+}
