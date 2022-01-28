d20ad4f8bc7a989178260839892eb5eecdb0e57d
kimjy1 <93234891+kimjy1@users.noreply.github.com>
2022-01-28 15:48:34 +0900

    다시 수정
    수정 완료

--- script/main.js
+++ script/main.js
@@ -60,30 +60,30 @@ $.ajax({
   // dl.documents[0].height 이미지의 세로 길이
 
   .done(function (dl) {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 8; i++) {
       $(".livevideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].image_url + "'/>" + "</a>");
     }
 
-    for (let i = 6; i < 7; i++) {
-      $(".livevideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].image_url + "'/>" + "</a>");
-    }
+    // for (let i = 6; i < 7; i++) {
+    //   $(".livevideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].image_url + "'/>" + "</a>");
+    // }
 
-    $(".livevideo").eq(4).append("<a href='#'>" + "<img src='" + dl.documents[8].image_url + "'/>" + "</a>");
+    // $(".livevideo").eq(4).append("<a href='#'>" + "<img src='" + dl.documents[8].image_url + "'/>" + "</a>");
   });
 
 // 오른쪽 섹션 - 인기 라이브 비디오&텍스트
-$.ajax({
-  method: "GET",
-  url: "https://dapi.kakao.com/v2/search/image",
-  data: { query: "킬링벌스" },
-  headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
-})
+// $.ajax({
+//   method: "GET",
+//   url: "https://dapi.kakao.com/v2/search/image",
+//   data: { query: "킬링벌스" },
+//   headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
+// })
 
-  .done(function (dl) {
-    $(".livevideo").eq(5).append("<a href='#'>" + "<img src='" + dl.documents[8].image_url + "'/>" + "</a>");
+//   .done(function (dl) {
+//     $(".livevideo").eq(5).append("<a href='#'>" + "<img src='" + dl.documents[8].image_url + "'/>" + "</a>");
 
-    $(".livevideo").eq(7).append("<a href='#'>" + "<img src='" + dl.documents[5].image_url + "'/>" + "</a>");
-  });
+//     $(".livevideo").eq(7).append("<a href='#'>" + "<img src='" + dl.documents[5].image_url + "'/>" + "</a>");
+//   });
 
 // 왼쪽 섹션 - 셀럽들의 라이프스타일 비디오&텍스트
 // $.ajax({
