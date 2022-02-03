cd9a37f2be8c12b803b679f68768346677c78651
kimjy1 <93234891+kimjy1@users.noreply.github.com>
2022-02-03 13:04:38 +0900

    메인 페이지 수정
    왼쪽 섹션
    - div 박스 크기 조절
    
    오른쪽 섹션
    - 제목 변경 완료
    - 이미지 변경 완료

--- script/main.js
+++ script/main.js
@@ -7,13 +7,6 @@ $.ajax({
 })
 
   .done(function (dl) {
-    // dl.documents[i].title 동영상 제목
-    // dl.documents[i].url 동영상 링크
-    // dl.documents[i].datetime 동영상 등록일, ISO 8601
-    // dl.documents[i].play_time 동영상 재생시간, 초 단위
-    // dl.documents[i].thumbnail 동영상 미리보기 URL
-    // dl.documents[i].author 동영상 업로더
-
     for (let i = 0; i < 4; i++) {
       $(".originalvideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].image_url + "'/>" + "</a>");
     }
@@ -48,11 +41,11 @@ $.ajax({
   });
 
 
-// 오른쪽 섹션 - 인기 라이브 비디오&텍스트
+// 오른쪽 섹션 - 인기 라이브 비디오1
 $.ajax({
   method: "GET",
   url: "https://dapi.kakao.com/v2/search/image",
-  data: { query: "며느라기2" },
+  data: { query: "유튜브 shorts", page: 1 },
   headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
 })
   // dl.documents[0].image_url 이미지 URL
@@ -60,41 +53,43 @@ $.ajax({
   // dl.documents[0].height 이미지의 세로 길이
 
   .done(function (dl) {
-    for (let i = 0; i < 8; i++) {
-      $(".livevideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].image_url + "'/>" + "</a>");
+    $(".livevideo").eq(0).append("<a href='#'>" + "<img src='" + dl.documents[38].image_url + "'/>" + "</a>");
+
+    $(".livevideo").eq(1).append("<a href='#'>" + "<img src='" + dl.documents[71].image_url + "'/>" + "</a>");
+
+    for (let i = 0; i < 12; i++) {
+      $(".livevideo .title").eq(i).append("<p>" + dl.documents[i].title + "</p>");
     }
-
-    // for (let i = 6; i < 7; i++) {
-    //   $(".livevideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].image_url + "'/>" + "</a>");
-    // }
-
-    // $(".livevideo").eq(4).append("<a href='#'>" + "<img src='" + dl.documents[8].image_url + "'/>" + "</a>");
   });
 
-// 오른쪽 섹션 - 인기 라이브 비디오&텍스트
-// $.ajax({
-//   method: "GET",
-//   url: "https://dapi.kakao.com/v2/search/image",
-//   data: { query: "킬링벌스" },
-//   headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
-// })
+// 오른쪽 섹션 - 인기 라이브 비디오2
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v2/search/image",
+  data: { query: "일소라", page: 1 },
+  headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
+})
+  // dl.documents[0].image_url 이미지 URL
+  // dl.documents[0].width 이미지의 가로 길이
+  // dl.documents[0].height 이미지의 세로 길이
 
-//   .done(function (dl) {
-//     $(".livevideo").eq(5).append("<a href='#'>" + "<img src='" + dl.documents[8].image_url + "'/>" + "</a>");
+  .done(function (dl) {
+    $(".livevideo").eq(2).append("<a href='#'>" + "<img src='" + dl.documents[67].image_url + "'/>" + "</a>");
 
-//     $(".livevideo").eq(7).append("<a href='#'>" + "<img src='" + dl.documents[5].image_url + "'/>" + "</a>");
-//   });
+    $(".livevideo").eq(3).append("<a href='#'>" + "<img src='" + dl.documents[75].image_url + "'/>" + "</a>");
+  });
 
-// 왼쪽 섹션 - 셀럽들의 라이프스타일 비디오&텍스트
-// $.ajax({
-//   method: "GET",
-//   url: "https://dapi.kakao.com/v2/search/vclip",
-//   data: { query: "셀럽들의 라이프스타일", size: 30 },
-//   headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
-// })
+// 오른쪽 섹션 - 인기 라이브 비디오3
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v2/search/image",
+  data: { query: "세로 비디오", page: 1 },
+  headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
+})
+  // dl.documents[0].image_url 이미지 URL
+  // dl.documents[0].width 이미지의 가로 길이
+  // dl.documents[0].height 이미지의 세로 길이
 
-//   .done(function (dl) {
-//     for (let i = 0; i < 12; i++) {
-//       $(".popularvideo").eq(i).append("<a href='#'>" + "<img src='" + dl.documents[i].thumbnail + "'/>" + "</a>");
-//     }
-//   });
+  .done(function (dl) {
+    $(".livevideo").eq(4).append("<a href='#'>" + "<img src='" + dl.documents[21].image_url + "'/>" + "</a>");
+  });
