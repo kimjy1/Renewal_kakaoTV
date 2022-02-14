0d96bfd5698e98c3d39bf41690bc282faacca559
kimjy1 <93234891+kimjy1@users.noreply.github.com>
2022-02-14 14:17:30 +0900

    메인 페이지 디자인 변경
    - 인기 오리지널 이미지 변경 완료
    - 숏폼 콘텐츠 이미지 추가 완료
    - html 태그 및 css 속성 변경 완료

--- script/main.js
+++ script/main.js
@@ -19,7 +19,7 @@ $.ajax({
 })
 
   .done(function (dl) {
-    $(".originalvideo").eq(1).append("<a href='#'>" + "<img src='" + dl.documents[53].image_url + "'/>" + "</a>");
+    $(".originalvideo").eq(1).append("<a href='#'>" + "<img src='" + dl.documents[54].image_url + "'/>" + "</a>");
   });
 
 // 왼쪽 섹션 - 인기 오리지널 비디오 이미지 3
@@ -46,22 +46,32 @@ $.ajax({
     $(".originalvideo").eq(3).append("<a href='#'>" + "<img src='" + dl.documents[24].image_url + "'/>" + "</a>");
   });
 
-// 왼쪽 섹션 - 인기 오리지널 비디오 이미지 5~7
+// 왼쪽 섹션 - 인기 오리지널 비디오 이미지 5
 $.ajax({
   method: "GET",
   contentType: "application/json; charset=UTF-8",
   url: "https://dapi.kakao.com/v2/search/image",
-  data: { query: "멜뮤로그", page: 1 },
+  data: { query: "카카오티비 그림자 미녀", page: 4 },
   headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
 })
 
   .done(function (dl) {
-    let imageindex = 4;
-    for (let i = 11; i < 13; i++) {
-      $(".originalvideo").eq(imageindex).append("<a href='#'>" + "<img src='" + dl.documents[i].image_url + "'/>" + "</a>");
-      imageindex++;
-    }
-    $(".originalvideo").eq(imageindex).append("<a href='#'>" + "<img src='" + dl.documents[14].image_url + "'/>" + "</a>");
+    $(".originalvideo").eq(4).append("<a href='#'>" + "<img src='" + dl.documents[70].image_url + "'/>" + "</a>");
+  });
+
+// 왼쪽 섹션 - 인기 오리지널 비디오 이미지 6~7
+$.ajax({
+  method: "GET",
+  contentType: "application/json; charset=UTF-8",
+  url: "https://dapi.kakao.com/v2/search/image",
+  data: { query: "카카오티비 파이트클럽", page: 1 },
+  headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
+})
+  // dl.documents[0].image_url 이미지 URL
+  // dl.documents[0].display_sitename 출처
+  .done(function (dl) {
+    $(".originalvideo").eq(5).append("<a href='#'>" + "<img src='" + dl.documents[19].image_url + "'/>" + "</a>");
+    $(".originalvideo").eq(6).append("<a href='#'>" + "<img src='" + dl.documents[25].image_url + "'/>" + "</a>");
   });
 
 // 왼쪽 섹션 - 인기 오리지널 비디오 이미지 8
@@ -69,12 +79,13 @@ $.ajax({
   method: "GET",
   contentType: "application/json; charset=UTF-8",
   url: "https://dapi.kakao.com/v2/search/image",
-  data: { query: "멜뮤로그", page: 1 },
+  data: { query: "카카오티비 개미는 오늘도 뚠뚠", page: 1 },
   headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
 })
-
+  // dl.documents[0].image_url 이미지 URL
+  // dl.documents[0].display_sitename 출처
   .done(function (dl) {
-    $(".originalvideo").eq(7).append("<a href='#'>" + "<img src='" + dl.documents[59].image_url + "'/>" + "</a>");
+    $(".originalvideo").eq(7).append("<a href='#'>" + "<img src='" + dl.documents[11].image_url + "'/>" + "</a>");
   });
 
 
@@ -90,48 +101,3 @@ $.ajax({
   .done(function (dl) {
     $(".originalvideo").eq(8).append("<a href='#'>" + "<img src='" + dl.documents[21].image_url + "'/>" + "</a>");
   });
-
-// // 왼쪽 섹션 - 인기 오리지널 비디오 이미지 9
-// $.ajax({
-//   method: "GET",
-//   contentType: "application/json; charset=UTF-8",
-//   url: "https://dapi.kakao.com/v2/search/image",
-//   data: { query: "카카오티비 사랑과 전쟁", page: 1 },
-//   headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
-// })
-//   // dl.documents[0].image_url 이미지 URL
-//   // dl.documents[0].display_sitename 출처
-//   // dl.documents[0].width 이미지의 가로 길이
-//   // dl.documents[0].height 이미지의 세로 길이
-
-//   .done(function (dl) {
-//     let imageindex = 0;
-//     for (let i = 21; i < 27; i++) {
-//       $(".originalvideo").eq(imageindex).append("<a href='#'>" + "<img src='" + dl.documents[i].image_url + "'/>" + "</a>");
-//       $(".original-list .title > p").eq(imageindex).append(dl.documents[i].display_sitename);
-//       imageindex++;
-//     }
-//   });
-
-
-// 오른쪽 섹션 - 인기 라이브 비디오
-// $.ajax({
-//   method: "GET",
-//   contentType: "application/json; charset=UTF-8",
-//   url: "https://dapi.kakao.com/v2/search/image",
-//   data: { query: "#쇼츠", page: 1 },
-//   headers: { Authorization: "KakaoAK 71d24c50e0b18ae03087bb26977587bc" }
-// })
-//   // dl.documents[0].image_url 이미지 URL
-//   // dl.documents[0].display_sitename 출처
-//   // dl.documents[0].width 이미지의 가로 길이
-//   // dl.documents[0].height 이미지의 세로 길이
-
-//   .done(function (dl) {
-//     let imageindex = 0;
-//     for (let i = 65; i < 73; i++) {
-//       $(".livevideo").eq(imageindex).append("<a href='#'>" + "<img src='" + dl.documents[i].image_url + "'/>" + "</a>");
-//       $(".livebox .title").eq(imageindex).append("<p>" + dl.documents[i].display_sitename + "</p>");
-//       imageindex++;
-//     }
-//   });
